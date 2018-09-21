1e602fffe2cc385e36d5886c13e95bfa14259bc7
Raunak Agrawal <raunk325@gmail.com>
2018-09-21 05:17:11 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import "./App.css";
 class App extends Component {
   state = {
     openedCard: null,
-    characters: characters
-    // hasFlippedCard: false
+    characters: characters,
+    lockBoard: false,
+    moves: 0,
+    countMin: 0,
+    countSec: 0,
+    tempCount: 0,
+    matchPairs: 0
   };
   componentWillMount() {
     function shuffleArray(array) {
@@ -15,104 +20,109 @@ class App extends Component {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
       }
-      // return array;
+      return array;
     }
     shuffleArray(this.state.characters);
   }
-  onMatchCards = (item, id) => {
-    // console.log(item, id);
-
-    const { openedCard, characters } = this.state;
-
-    var index = this.state.characters.findIndex(a => {
-      return a.id === id;
+  timerFunc = () => {
+    this.myInterval = setInterval(() => {
+      this.setState({
+        countSec: this.state.countSec + 1
+      });
+      if (this.state.countSec % 60 === 0) {
+        this.setState({
+          countSec: 0,
+          countMin: this.state.countMin + 1
+        });
+      }
+    }, 1000);
+  };
+  timerCall = () => {
+    this.setState(
+      state => {
+        return (state.tempCount = this.state.tempCount + 1);
+      },
+      _ => {
+        if (this.state.tempCount === 1) {
+          this.timerFunc();
+        }
+      }
+    );
+  };
+  onRestart = () => {
+    characters.map(item => (item.opened = false));
+    this.setState({
+      moves: 0,
+      countMin: 0,
+      countSec: 0,
+      openedCard: null,
+      characters: characters,
+      tempCount: 0
     });
+    clearInterval(this.myInterval);
+  };
+
+  onMatchCards = (imageItem, id) => {
+    const index = this.state.characters.findIndex(a => a.id === id);
 
     this.setState(
       state => {
-        state.characters[index].opened = true;
+        return (state.characters[index].opened = true);
       },
       _ => {
-        debugger;
         if (!this.state.openedCard) {
-          // debugger;
-
-          this.setState(state => {
-            state.openedCard = item;
-          });
+          this.setState(
+            state => {
+              state.openedCard = {
+                imageItem: imageItem,
+                id: id
+              };
+            },
+            _ => {
+              this.timerCall();
+            }
+          );
         } else {
-          // stateJSON.characters[index].opened = true;
-          debugger;
-          if (this.state.openedCard === item) {
-            console.log("MATCH");
-
-            this.setState(state => {
-              state.openedCard = null;
-            });
-            // this.setState({
-            //   stateJSON
-            //   // console.log(this.state.openedCard, id);
-            // });
-
-            // this.setState({ openedCard: null });
+          if (this.state.openedCard.imageItem === imageItem) {
+            console.log("match");
+            console.log(this.state.openedCard.id, id);
+            this.setState(
+              state => {
+                state.openedCard = null;
+                state.moves = state.moves + 1;
+                state.matchPairs++;
+                return state;
+              },
+              _ => {
+                if (this.state.matchPairs === 6) {
+                  this.onRestart();
+                }
+              }
+            );
           } else {
-            console.log("not match");
-            // stateJSON.characters[index].opened = false;
-            // setTimeout(this.setState(stateJSON), 1500);
+            this.setState({ moves: this.state.moves + 1 });
+            var firstCardIndex = this.state.characters.findIndex(
+              a => a.id === id
+            );
+            var SecondCardIndex = this.state.characters.findIndex(
+              a => a.id === this.state.openedCard.id
+            );
+
+            setTimeout(() => {
+              this.setState(state => {
+                state.characters[SecondCardIndex].opened = false;
+                state.characters[firstCardIndex].opened = false;
+                // state.moves++;
+                state.openedCard = null;
+                return state;
+              });
+            }, 500);
           }
         }
       }
     );
   };
-  // else {
-  //     // this.setState({ secondCard: item }, _ => {
-  //     //   console.log(this.state.secondCard, id);
-  //     //   this.check();
-  //     // });
-  //     // if(match occurs){
-  //     stateJSON.openedCard = null;
-  //     this.setState(stateJSON);
-  //     // } else {
-  //     //   // jo upar waalo ko openkia  tha unhe  close kar do settimeoutme
-  //     // }
-  //   }
 
-  // check() {
-  //   if (this.state.openedCard === this.state.secondCard) {
-  //     console.log("match");
-  //     this.setState({ opened: true });
-  //   } else {
-  //     console.log("Not match");
-  //     this.setState({ opened: false });
-  //   }
-  // }
-
-  //   onFlipCards = (item, id) => {
-  //     let oldState = this.state;
-
-  //     if (!openedCard) {
-  //       this.setState({ openedCard: item }, _ => {
-  //         console.log(this.state.openedCard);
-  //         // debugger;
-  //         oldState.characters[id - 1].open = true;
-  //         this.setState({
-  //           oldState
-  //         });
-  //       });
-  //     } else {
-  //     }
-  //     // const { firstCard, secondCard, hasFlippedCard } = this.state;
-  //     // // if (item == firstCard) return;
-  //     // console.log(item);
-  //     // if (!hasFlippedCard) {
-  //     //   this.setState({ hasFlippedCard: true, firstCard: item });
-  //     //   console.log(firstCard);
-  //     // } else {
-  //     //   this.setState({ hasFlippedCard: false, secondCard: item });
-  //     //   console.log(secondCard);
-  //     //   // checkForMatch();
-  //     // }
-  //   };
   render() {
     return (
       <div className="container">
@@ -124,15 +134,21 @@ class App extends Component {
             image={character.image}
             onMatch={this.onMatchCards}
             opened={character.opened}
-            matched={character.matched}
-            // open={character.open}
-            // match={this.match}
-            // flipcard={this.flipcard}
           />
         ))}
+        <span className="button">Moves: {this.state.moves}</span>
+        <span className="button">
+          Time: {this.state.countMin}
+          min:
+          {this.state.countSec}
+          sec
+        </span>
+
+        <button onClick={this.onRestart} className="button">
+          Restart
+        </button>
       </div>
     );
   }
-  // }
 }
 export default App;
--- src/Icon.js
+++ src/Icon.js
@@ -1,39 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 class Icon extends Component {
-  state = {
-    // hasFlippedCard: false
-    // showImg: Array(this.props.character.length).fill("hidden")
-    //   firstCard: "",
-    //   secondCard: ""
-  };
   flipcard(e) {
-    //     // const firstCard = this.props.character.charName;
     this.props.onMatch(this.props.character.charName, this.props.character.id);
   }
-  //   //   if (this === this.state.firstCard) return;
-  //   //   if (!this.state.hasFlippedCard) {
-  //   //     this.setState({ hasFlippedCard: true, firstCard: this.props.charName });
-  //   //   } else {
-  //   //     this.setState({ secondCard: this.props.charName, hasFlippedCard: false });
-  //   //     setTimeout(() => {
-  //   //       this.checkforMatch();
-  //   //     }, 1000);
-  //   //   }
-  //   // };
-  //   // checkforMatch = () =>
-  //   //   this.state.firstCard === this.state.secondCard
-  //   //     ? console.log("WON")
-  //   //     : console.log("LOST");
+
   render() {
     return (
-      <div className="memory-card" onClick={_ => this.flipcard()}>
+      <div
+        className="memory-card"
+        onClick={e => {
+          this.props.opened ? null : this.flipcard();
+        }}
+      >
         <img
           style={{
             visibility: this.props.opened ? "visible" : "hidden"
           }}
-          // className={{ style: "backface-visibility: hidden;" }}
-          // charName={this.props.charName}
           className="card1"
           alt={this.props.charName}
           src={this.props.image}
